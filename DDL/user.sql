ca2c77fe599ef7a9d609a013fc848a5d8935503c
Betty-DOS-SANTOS-PEREIRA <dspbetty@gmail.com>
2023-07-11 11:50:24 +0200

    table user

---
+++ DDL/user.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+DROP TABLE IF EXISTS "user";
+
+CREATE TABLE "user"(
+  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "pseudo" text UNIQUE NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL
+);
+
+COMMIT;
