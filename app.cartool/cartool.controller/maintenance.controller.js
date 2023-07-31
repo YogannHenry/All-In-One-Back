a14568d8c0bbfb58f286047c100bf90b9000f70d
Betty-DOS-SANTOS-PEREIRA <dspbetty@gmail.com>
2023-07-31 11:06:34 +0200

    ajout calcul de date pour getAllMaintenanceByCarId

--- app.cartool/cartool.controller/maintenance.controller.js
+++ app.cartool/cartool.controller/maintenance.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable max-len */
 /* eslint-disable camelcase */
 const dayjs = require('dayjs');
@@ -23,7 +24,28 @@ const maintenanceController = {
     if (allMaintenance.length === 0) {
       res.status(404).json(`message: il n'existe aucun entretien pour la voiture avec l'id ${carId} `);
     }
-    res.json(allMaintenance);
+    const { current_km } = car[0];
+    const allMaintenanceCalcul = [];
+    for (const oneMaintenance of allMaintenance) {
+      const {
+        last_date_verif, last_km_verif, validity_period, validity_km,
+      } = oneMaintenance;
+
+      // calcul des km restants
+      const lastKmRemaining = last_km_verif + validity_km - current_km;
+
+      // calcul du temps restant avant entretien
+      const lastDate = dayjs();
+      const resultDate = lastDate
+        .add(validity_period.years || 0, 'years')
+        .add(validity_period.months || 0, 'months')
+        .add(validity_period.days || 0, 'days');
+      const lastTimeRemaining = resultDate.toISOString();
+      const oneMaintenanceCalcul = { ...oneMaintenance, lastKmRemaining, lastTimeRemaining };
+      allMaintenanceCalcul.push(oneMaintenanceCalcul);
+    }
+
+    res.json(allMaintenanceCalcul);
   },
 
   async getOneMaintenance(req, res) {
